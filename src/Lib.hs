07916b6549a09a2a16673661b9c01590edba534e
0rphee <0rph3e@proton.me>
2023-04-09 15:06:49 -0600

    add yaml parsing functions, remove brick stuff

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,26 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Lib where
+module Lib (someFunc) where
 
+import qualified Data.Vector as V
 import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-import Debug.Trace
+import Data.Yaml
+    ( (.:),
+      decodeFileEither,
+      FromJSON(..),
+      Value(..),
+      ParseException, prettyPrintParseException )
+import Data.Aeson.Types
+    ( typeMismatch, parseFail, prependFailure)
+
+someFunc :: IO ()
+someFunc = do
+  (res :: Either ParseException [Subject]) <- decodeFileEither "test.yaml"
+  case res of
+    Left err   -> putStrLn $ prettyPrintParseException err
+    Right result -> print result
 
 data Class
   = MondayClass    {classInterval :: Interval}
@@ -15,19 +30,64 @@ data Class
   | FridayClass    {classInterval :: Interval}
   | SaturdayClass  {classInterval :: Interval}
   | SundayClass    {classInterval :: Interval}
-  deriving (Show)
+
+instance Show Class where
+  show x 
+    = case x of
+        MondayClass    inter -> "Monday: " <> show inter
+        TuesdayClass   inter -> "Monday: " <> show inter
+        WednesdayClass inter -> "Monday: " <> show inter
+        ThursdayClass  inter -> "Monday: " <> show inter
+        FridayClass    inter -> "Monday: " <> show inter
+        SaturdayClass  inter -> "Monday: " <> show inter
+        SundayClass    inter -> "Monday: " <> show inter
+      
 
 data Hour
   = H0   | H1   | H2  | H3  | H4  | H5
   | H6   | H7   | H8  | H9  | H10 | H11
   | H12  | H13  | H14 | H15 | H16 | H17
   | H18  | H19  | H20 | H21 | H22 | H23
-  deriving (Show, Eq, Ord)
+  deriving (Eq, Ord)
+
+instance Show Hour where
+ show x 
+  = case x of 
+    H0  -> "00"
+    H1  -> "01"
+    H2  -> "02"
+    H3  -> "03"
+    H4  -> "04"
+    H5  -> "05"
+    H6  -> "06"
+    H7  -> "07"
+    H8  -> "08"
+    H9  -> "09"
+    H10 -> "10"
+    H11 -> "11"
+    H12 -> "12"
+    H13 -> "13"
+    H14 -> "14"
+    H15 -> "15"
+    H16 -> "16"
+    H17 -> "17"
+    H18 -> "18"
+    H19 -> "19"
+    H20 -> "20"
+    H21 -> "21"
+    H22 -> "22"
+    H23 -> "23"
 
 data Minute
   = ZeroMinutes
   | HalfAnHour
-  deriving (Show, Eq, Ord)
+  deriving (Eq, Ord)
+
+instance Show Minute where
+  show x 
+    = case x of
+      ZeroMinutes -> "00"
+      HalfAnHour  -> "30"
 
 data Time
   = MkTime
@@ -35,37 +95,63 @@ data Time
     timeHour   :: Hour
   , timeMinute :: Minute
   }
-  deriving (Show, Eq, Ord)
+  deriving (Eq, Ord)
+
+instance Show Time where
+  show (MkTime hour minutes) = show hour <> ":" <> show minutes
+
 
 data Interval
  = MkInterval
     {
-      startingTime :: Time
-    , endTime      :: Time
+      intervalStartingTime :: Time
+    , intervalEndTime      :: Time
     }
-    deriving (Show)
+
+instance Show Interval where
+  show (MkInterval beginning end) = show beginning <> "-" <> show end
 
 data Subject
    = MkSubject
      {
-       subjName    :: T.Text
-     , subjClasses :: [Class]
+       subjName      :: T.Text
+     , subjID        :: T.Text
+     , subjProfessor :: T.Text
+     , subjClasses   :: [Class]
      }
-    deriving (Show)
+instance Show Subject where
+  show (MkSubject sname sid sprof sclasses) = "( Sname: " <> show sname 
+                                           <> ", Sid: " <> show sid 
+                                           <> ", Sprof: " <> show sprof 
+                                           <> ", Sclasses: " <> show sclasses 
+                                           <> ")"
+
+
 
 createInterval :: Time -> Time -> Maybe Interval
 createInterval x y = if x < y
                      then Just $ MkInterval x y
                      else Nothing
 
-intervalsOverlap :: Interval -> Interval -> Maybe Interval
-intervalsOverlap (MkInterval a b) inter2@(MkInterval x y)
-  | a <= x && x <= b = Nothing
-  | a <= y && y <= b = Nothing
-  | x <= a && a <= y = Nothing
-  | x <= b && b <= y = Nothing
-  | otherwise      = Just inter2
+intervalsOverlap :: Interval -> Interval -> Bool
+intervalsOverlap (MkInterval a b) (MkInterval x y)
+  | a <= x && x <= b = True
+  | a <= y && y <= b = True
+  | x <= a && a <= y = True
+  | x <= b && b <= y = True
+  | otherwise        = False
 
+clasesOverlap :: Class -> Class -> Bool
+clasesOverlap class1 class2 
+  = case (class1, class2) of
+      (MondayClass inter1, MondayClass inter2)       -> intervalsOverlap inter1 inter2
+      (TuesdayClass inter1, TuesdayClass inter2)     -> intervalsOverlap inter1 inter2
+      (WednesdayClass inter1, WednesdayClass inter2) -> intervalsOverlap inter1 inter2
+      (ThursdayClass inter1, ThursdayClass inter2)   -> intervalsOverlap inter1 inter2
+      (FridayClass inter1, FridayClass inter2)       -> intervalsOverlap inter1 inter2
+      (SaturdayClass inter1, SaturdayClass inter2)   -> intervalsOverlap inter1 inter2
+      _                                              -> False
+      
 validateIntervals :: [Interval] -> Maybe [Interval]
 validateIntervals ls = go ls []
   where go :: [Interval] -> [Interval] -> Maybe [Interval]
@@ -82,46 +168,126 @@ validateIntervals ls = go ls []
 validateDay :: [Class] -> Maybe [Class]
 validateDay classes = do
   let intervals = fmap classInterval classes 
-  _ <- validateIntervals intervals
-  pure classes
-
+  case validateIntervals intervals of
+    Just _ -> pure classes
+    _      -> Nothing
 
 type ClassColl = ([Class], [Class], [Class], [Class], [Class], [Class], [Class]) 
 
--- createSubject :: T.Text -> [Class] -> Maybe Subject 
--- createSubject name classes = 
---   where go :: [Class] -> ClassColl -> ClassColl
---         go [] a = a
---         go (x:xs) (a, b, c, d, e, f, g) 
---           = case x of
---               MondayClass _    -> (x:a, b, c, d, e, f, g)
---               TuesdayClass _   -> (a, x:b, c, d, e, f, g)
---               WednesdayClass _ -> (a, b, x:c, d, e, f, g)
---               ThursdayClass _  -> (a, b, c, x:d, e, f, g)
---               FridayClass _    -> (a, b, c, d, x:e, f, g)
---               SaturdayClass _  -> (a, b, c, d, e, x:f, g)
---               SundayClass _    -> (a, b, c, d, e, f, x:g)
+-- c = MkInterval ( MkTime H10 ZeroMinutes ) (MkTime H11 HalfAnHour)
+-- d = MkInterval ( MkTime H10 HalfAnHour ) (MkTime H12 ZeroMinutes)
+
+-- firstI = MkInterval (MkTime H10 ZeroMinutes) (MkTime H12 HalfAnHour)
+-- sedonI = MkInterval (MkTime H0 HalfAnHour)  (MkTime H2 ZeroMinutes)
+
+-- t1 = MkInterval (MkTime H12 HalfAnHour)  (MkTime H13 ZeroMinutes)
+-- t2 = MkInterval (MkTime H15 ZeroMinutes) (MkTime H16 HalfAnHour)
+-- t3 = MkInterval (MkTime H16 HalfAnHour)  (MkTime H17 ZeroMinutes)
+-- t4 = MkInterval (MkTime H18 ZeroMinutes) (MkTime H18 HalfAnHour)
+-- t5 = MkInterval (MkTime H11 HalfAnHour)  (MkTime H12 ZeroMinutes)
+
+-- c1 = TuesdayClass firstI 
+-- c2 = TuesdayClass sedonI
+-- c3 = TuesdayClass c
+-- c4 = TuesdayClass d
 
 
-c = MkInterval ( MkTime H10 ZeroMinutes ) (MkTime H11 HalfAnHour)
-d = MkInterval ( MkTime H10 HalfAnHour ) (MkTime H12 ZeroMinutes)
+instance FromJSON Time where
+  parseJSON (String str) 
+     = prependFailure "parsing Time failed, " $ case res of
+       [x,y] -> MkTime <$> h x <*> t y 
+       [] -> parseFail "inexistent hour for class"
+       _ -> parseFail "unexpected ':'. More than 1."
+    where res =  T.splitOn ":" str
+          h head' = case head' of
+                      "00" -> pure H0
+                      "01" -> pure H1
+                      "02" -> pure H2
+                      "03" -> pure H3
+                      "04" -> pure H4
+                      "05" -> pure H5
+                      "06" -> pure H6
+                      "07" -> pure H7
+                      "08" -> pure H8
+                      "09" -> pure H9
+                      "10" -> pure H10
+                      "11" -> pure H1
+                      "12" -> pure H2
+                      "13" -> pure H3
+                      "14" -> pure H4
+                      "15" -> pure H5
+                      "16" -> pure H6
+                      "17" -> pure H7
+                      "18" -> pure H8
+                      "19" -> pure H9
+                      "20" -> pure H10
+                      "21" -> pure H1
+                      "22" -> pure H2
+                      "23" -> pure H3
+                      _    -> parseFail $ "Invalid hour: " <> T.unpack head'
+          t tail' = case tail' of
+                      "00" -> pure ZeroMinutes
+                      "30" -> pure HalfAnHour
+                      _    -> parseFail $ "Invalid minutes: " <> T.unpack tail'
+  parseJSON invalid =
+        prependFailure "parsing Time failed, "
+            (typeMismatch "String" invalid) 
 
-firstI = MkInterval (MkTime H10 ZeroMinutes) (MkTime H12 HalfAnHour)
-sedonI = MkInterval (MkTime H0 HalfAnHour)  (MkTime H2 ZeroMinutes)
+instance FromJSON Interval where
+  parseJSON (Object obj) = do
+    beginningTime <- obj .: "inicio" 
+    endTime       <- obj .: "final"
+    case createInterval beginningTime endTime of
+      Nothing              -> 
+                  parseFail $ "invalid Interval, class ends: " 
+                            <> show beginningTime 
+                            <> ", before it begins: "
+                            <> show endTime
+      Just validInterval -> pure validInterval
+  parseJSON invalid =
+        prependFailure "parsing Interval failed, "
+            (typeMismatch "Object" invalid) 
 
-t1 = MkInterval (MkTime H12 HalfAnHour)  (MkTime H13 ZeroMinutes)
-t2 = MkInterval (MkTime H15 ZeroMinutes) (MkTime H16 HalfAnHour)
-t3 = MkInterval (MkTime H16 HalfAnHour)  (MkTime H17 ZeroMinutes)
-t4 = MkInterval (MkTime H18 ZeroMinutes) (MkTime H18 HalfAnHour)
-t5 = MkInterval (MkTime H11 HalfAnHour)  (MkTime H12 ZeroMinutes)
+instance FromJSON Class where
+  parseJSON (Object obj) = prependFailure "parsing Class failed, " $ do
+    day      <- obj .: "dia" 
+    interval <- prependFailure ("in day '" <> T.unpack day <> "', ") $ parseJSON (Object obj)
+    case day of
+      "lunes"     -> pure $ MondayClass interval
+      "martes"    -> pure $ TuesdayClass interval
+      "miercoles" -> pure $ WednesdayClass interval
+      "jueves"    -> pure $ ThursdayClass interval
+      "viernes"   -> pure $ FridayClass interval
+      _           -> parseFail $ "Invalid Class day: " <> T.unpack day
+  parseJSON invalid =
+        prependFailure "parsing Interval failed, "
+            (typeMismatch "Object" invalid) 
 
-c1 = TuesdayClass firstI 
-c2 = TuesdayClass sedonI
-c3 = TuesdayClass c
-c4 = TuesdayClass d
+  parseJSONList (Array (arr :: V.Vector Value)) 
+    = V.toList <$> traverse parseJSON arr
+  parseJSONList invalid =
+        prependFailure "parsing Interval failed, "
+            (typeMismatch "Array" invalid) 
 
-someFunc :: IO ()
-someFunc = TIO.putStrLn "someFunc"
+instance FromJSON Subject where
+  parseJSON (Object obj) = prependFailure "parsing Subject failed, " $ do
+    name      <- obj .: "nombre"
+    let errorInClassName = "in class '" <> T.unpack name <> "', "
+    classId   <- prependFailure errorInClassName $ obj .: "id-clase"
+    let errorInClassId = errorInClassName 
+                      <> "with ID '" <> T.unpack classId <> "', " 
+    professor <- prependFailure errorInClassId $ obj .: "profesor"
+    let errorInClassProfessor = errorInClassId 
+                              <> ("with Professor: '" <> T.unpack professor <> "', ")
+    classes   <- prependFailure errorInClassProfessor   $ obj .: "dias"
+    pure $ MkSubject name classId professor classes
 
-
-
+    
+  parseJSON invalid =
+        prependFailure "parsing Interval failed, "
+            (typeMismatch "Object" invalid) 
+  parseJSONList (Array (arr :: V.Vector Value)) 
+    = V.toList <$> traverse parseJSON arr
+  parseJSONList invalid =
+        prependFailure "parsing Subjects failed, "
+            (typeMismatch "Array" invalid) 
